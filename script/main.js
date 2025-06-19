08bf40b490f4901762792ac90cf29df72a8bba73
jeonjaehee528 <jeon_jaehee@naver.com>
2025-06-19 17:22:07 +0900

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,19 @@
+$(".p1").click(function () {
+  $(".pop1").show();
+});
+$("button").click(function () {
+  $(".pop1").hide();
+  $(".pop2").hide();
+  $(".pop3").hide();
+  $(".pop4").hide();
+});
+
+$(".p2").click(function () {
+  $(".pop2").show();
+});
+$(".p3").click(function () {
+  $(".pop3").show();
+});
+$(".p4").click(function () {
+  $(".pop4").show();
+});
